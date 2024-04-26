5b49edbb21258c07938ec9e760bd2cbdfd540e1e
adsh <ad.sh.1997@mail.ru>
2024-04-26 13:32:52 +0500

    server pustoy

--- components/cards/admin-direction.card.tsx
+++ components/cards/admin-direction.card.tsx
@@ -14,18 +14,18 @@ function AdminDirectionCard({ direction }: Props) {
 			<div className='flex flex-col space-y-2 rounded-md bg-background p-2'>
 				<div className='relative h-52 w-full'>
 					<Image
-						src={direction.previewImage}
-						alt={direction.title}
+						src={direction!.previewImage}
+						alt={direction!.title}
 						fill
 						className='rounded-md object-cover'
 					/>
 				</div>
 				<div className='flex items-center justify-between px-2'>
 					<h1 className='font-space-grotesk text-2xl font-bold'>
-						{direction.title}
+						{direction!.title}
 					</h1>
-					<Badge variant={direction.published ? 'default' : 'destructive'}>
-						{direction.published ? 'Published' : 'Draft'}
+					<Badge variant={direction!.published ? 'default' : 'destructive'}>
+						{direction!.published ? 'Published' : 'Draft'}
 					</Badge>
 				</div>
 			</div>
