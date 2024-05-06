aa889663354691b8df5ef220d324ce51440440f9
adsh <ad.sh.1997@mail.ru>
2024-05-06 14:23:07 +0500

    Tell number string

--- lib/validation.ts
+++ lib/validation.ts
@@ -7,7 +7,7 @@ import { z } from 'zod'
 export const contactSchema = z.object({
 	message: z.string().min(10),
 	tell: z.string().min(9).max(12),
-	name: z.string().min(3),
+	fish: z.string().min(3),
 })
 
 export const courseSchema = z.object({
