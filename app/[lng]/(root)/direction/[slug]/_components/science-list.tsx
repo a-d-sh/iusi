76146c52717b5c3ff9701f79ff39aec4a05b2729
adsh <ad.sh.1997@mail.ru>
2024-05-06 13:07:09 +0500

    Card in Drawer

--- app/[lng]/(root)/direction/[slug]/_components/science-list.tsx
+++ app/[lng]/(root)/direction/[slug]/_components/science-list.tsx
@@ -20,38 +20,39 @@ function ScienceList(science: IScience) {
 
 	return (
 		<div className='mt-2 grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4'>
-			<Card className='group w-full'>
-				<CardContent className='relative h-56 w-full'>
-					<CustomImage src='/logo.png' alt={science.title} />
-				</CardContent>
-				<div className='my-4 flex flex-col space-y-2 px-2'>
-					<Separator />
-					<h2 className='line-clamp-2 font-space-grotesk text-2xl font-bold'>
-						{science.title}
-					</h2>
-					<Separator />
-					<p className='line-clamp-1 font-space-grotesk text-2xl font-bold'>
-						{science.books.length} {t('books')}
-					</p>
-					<Drawer>
-						<DrawerTrigger>Open</DrawerTrigger>
-						<DrawerContent>
-							<DrawerHeader>
-								<DrawerTitle>
-									<div className='mt-2 border-l-2 border-l-gray-800 p-4'>
-										{science.books.map(book => (
-											<BookList key={book._id} {...book} />
-										))}
-									</div>
-								</DrawerTitle>
-							</DrawerHeader>
-							<DrawerFooter>
-								<DrawerClose></DrawerClose>
-							</DrawerFooter>
-						</DrawerContent>
-					</Drawer>
-				</div>
-			</Card>
+			<Drawer>
+				<DrawerTrigger>
+					<Card className='group w-full'>
+						<CardContent className='relative h-56 w-full'>
+							<CustomImage src='/logo.png' alt={science.title} />
+						</CardContent>
+						<div className='my-4 flex flex-col space-y-2 px-2'>
+							<Separator />
+							<h2 className='line-clamp-2 font-space-grotesk text-2xl font-bold'>
+								{science.title}
+							</h2>
+							<Separator />
+							<p className='line-clamp-1 font-space-grotesk text-2xl font-bold'>
+								{science.books.length} {t('books')}
+							</p>
+						</div>
+					</Card>
+				</DrawerTrigger>
+				<DrawerContent>
+					<DrawerHeader>
+						<DrawerTitle>
+							<div className='mt-2 border-l-2 border-l-gray-800 p-4'>
+								{science.books.map(book => (
+									<BookList key={book._id} {...book} />
+								))}
+							</div>
+						</DrawerTitle>
+					</DrawerHeader>
+					<DrawerFooter>
+						<DrawerClose></DrawerClose>
+					</DrawerFooter>
+				</DrawerContent>
+			</Drawer>
 		</div>
 	)
 }
