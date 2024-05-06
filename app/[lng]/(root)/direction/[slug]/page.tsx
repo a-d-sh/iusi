cb953b9040432e731965747392c4a2464ed7333e
adsh <ad.sh.1997@mail.ru>
2024-05-06 12:10:02 +0500

    no grid

--- app/[lng]/(root)/direction/[slug]/page.tsx
+++ app/[lng]/(root)/direction/[slug]/page.tsx
@@ -33,12 +33,8 @@ async function Page({ params: { lng, slug } }: Props) {
 			<TopBar label='Elektron Kutubxona' extra={direction.title} />
 
 			<div className='container mx-auto max-w-6xl'>
-				<div className='grid grid-cols-3 gap-4 pt-12'>
-					<div className='col-span-2 max-lg:col-span-3'>
-						<Hero {...direction} />
-						<Overview {...direction} />
-					</div>
-				</div>
+				<Hero {...direction} />
+				<Overview {...direction} />
 			</div>
 		</>
 	)
