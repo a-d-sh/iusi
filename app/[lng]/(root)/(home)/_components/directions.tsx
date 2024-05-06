fbcbff1cbf5339aa5b42388b5f1b3cf0403677c6
adsh <ad.sh.1997@mail.ru>
2024-05-06 14:31:53 +0500

    no directions

--- app/[lng]/(root)/(home)/_components/directions.tsx
+++ app/[lng]/(root)/(home)/_components/directions.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+// import DirectionCard from '@/components/cards/direction.card'
+// import { directions } from '@/constants/index'
 import useTranslate from '@/hooks/use-translate'
 
 function Directions() {
@@ -7,16 +9,16 @@ function Directions() {
 
 	return (
 		<div className='container mx-auto max-w-6xl py-12'>
-			<div className='flex flex-col space-y-1'>
+			{/* <div className='flex flex-col space-y-1'>
 				<h1 className='text-center font-space-grotesk text-3xl font-bold'>
 					{t('topDirections')}
 				</h1>
 			</div>
 			<div className='mt-6 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4'>
-				{/* {directions.map(direction => (
-					<DirectionsCard key={direction.title} {...direction} />
-				))} */}
-			</div>
+				{directions.map(direction => (
+					<DirectionCard key={direction.title} {...direction} />
+				))}
+			</div> */}
 		</div>
 	)
 }
