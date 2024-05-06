22fb95b704cc3940d0dde0a2e8cd3de7788a20ca
adsh <ad.sh.1997@mail.ru>
2024-05-06 11:33:25 +0500

    asd

--- app/[lng]/(root)/direction/[slug]/page.tsx
+++ app/[lng]/(root)/direction/[slug]/page.tsx
@@ -1,19 +1,5 @@
-import {
-	getDetailedDirection,
-	getFeaturedDirections,
-} from '@/actions/direction.action'
-import { IDirection } from '@/app.types'
-import DirectionCard from '@/components/cards/direction.card'
+import { getDetailedDirection } from '@/actions/direction.action'
 import TopBar from '@/components/shared/top-bar'
-import {
-	Carousel,
-	CarouselContent,
-	CarouselItem,
-	CarouselNext,
-	CarouselPrevious,
-} from '@/components/ui/carousel'
-import { Separator } from '@/components/ui/separator'
-import { translation } from '@/i18n/server'
 import { Metadata, ResolvingMetadata } from 'next'
 import Hero from './_components/hero'
 import Overview from './_components/overview'
@@ -38,17 +24,13 @@ interface Props {
 	params: { lng: string; slug: string }
 }
 async function Page({ params: { lng, slug } }: Props) {
-	const { t } = await translation(lng)
-
 	const directionJSON = await getDetailedDirection(slug)
-	const directionsJSON = await getFeaturedDirections()
 
 	const direction = JSON.parse(JSON.stringify(directionJSON))
-	const directions = JSON.parse(JSON.stringify(directionsJSON))
 
 	return (
 		<>
-			<TopBar label='allDirections' extra='Full Directions ReactJS' />
+			<TopBar label='Elektron Kutubxona' extra={direction.title} />
 
 			<div className='container mx-auto max-w-6xl'>
 				<div className='grid grid-cols-3 gap-4 pt-12'>
@@ -57,27 +39,6 @@ async function Page({ params: { lng, slug } }: Props) {
 						<Overview {...direction} />
 					</div>
 				</div>
-
-				<Separator className='my-12' />
-
-				<h1 className='font-space-grotesk text-4xl font-bold'>
-					{t('youMayLike')}
-				</h1>
-
-				<Carousel opts={{ align: 'start' }} className='mt-6 w-full'>
-					<CarouselContent className='w-full'>
-						{directions.map((direction: IDirection) => (
-							<CarouselItem
-								key={direction.title}
-								className='md:basis-1/2 lg:basis-1/3'
-							>
-								<DirectionCard {...direction} />
-							</CarouselItem>
-						))}
-					</CarouselContent>
-					<CarouselPrevious />
-					<CarouselNext />
-				</Carousel>
 			</div>
 		</>
 	)
