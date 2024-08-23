67434b3bec1da08ec138bacf023ffe1a054df2c8
adsh <ad.sh.1997@mail.ru>
2024-08-23 15:41:08 +0500

    first multi step

--- app/[lng]/profile/(dashboard)/page.tsx
+++ app/[lng]/profile/(dashboard)/page.tsx
@@ -78,6 +78,21 @@ async function Page({ params }: LngParams) {
 							<h3 className='font-medium leading-tight'>User info</h3>
 							<p className='text-sm'>Step details here</p>
 						</span>
+						<svg
+							className='w-3 h-3 ms-2 sm:ms-4 rtl:rotate-180'
+							aria-hidden='true'
+							xmlns='http://www.w3.org/2000/svg'
+							fill='none'
+							viewBox='0 0 12 10'
+						>
+							<path
+								stroke='currentColor'
+								stroke-linecap='round'
+								stroke-linejoin='round'
+								stroke-width='2'
+								d='m7 9 4-4-4-4M1 9l4-4-4-4'
+							/>
+						</svg>
 					</li>
 					<li className='flex items-center text-gray-500 dark:text-gray-400 space-x-2.5 rtl:space-x-reverse'>
 						<span className='flex items-center justify-center w-8 h-8 border border-gray-500 rounded-full shrink-0 dark:border-gray-400'>
@@ -87,6 +102,21 @@ async function Page({ params }: LngParams) {
 							<h3 className='font-medium leading-tight'>Company info</h3>
 							<p className='text-sm'>Step details here</p>
 						</span>
+						<svg
+							className='w-3 h-3 ms-2 sm:ms-4 rtl:rotate-180'
+							aria-hidden='true'
+							xmlns='http://www.w3.org/2000/svg'
+							fill='none'
+							viewBox='0 0 12 10'
+						>
+							<path
+								stroke='currentColor'
+								stroke-linecap='round'
+								stroke-linejoin='round'
+								stroke-width='2'
+								d='m7 9 4-4-4-4M1 9l4-4-4-4'
+							/>
+						</svg>
 					</li>
 					<li className='flex items-center text-gray-500 dark:text-gray-400 space-x-2.5 rtl:space-x-reverse'>
 						<span className='flex items-center justify-center w-8 h-8 border border-gray-500 rounded-full shrink-0 dark:border-gray-400'>
@@ -96,15 +126,6 @@ async function Page({ params }: LngParams) {
 							<h3 className='font-medium leading-tight'>Payment info</h3>
 							<p className='text-sm'>Step details here</p>
 						</span>
-					</li>
-				</ol>
-
-				<ol className='flex items-center w-full p-3 space-x-2 text-sm font-medium text-center text-gray-500 bg-white border border-gray-200 rounded-lg shadow-sm dark:text-gray-400 sm:text-base dark:bg-gray-800 dark:border-gray-700 sm:p-4 sm:space-x-4 rtl:space-x-reverse'>
-					<li className='flex items-center text-blue-600 dark:text-blue-500'>
-						<span className='flex items-center justify-center w-5 h-5 me-2 text-xs border border-blue-600 rounded-full shrink-0 dark:border-blue-500'>
-							1
-						</span>
-						Personal <span className='hidden sm:inline-flex sm:ms-2'>Info</span>
 						<svg
 							className='w-3 h-3 ms-2 sm:ms-4 rtl:rotate-180'
 							aria-hidden='true'
@@ -120,33 +141,6 @@ async function Page({ params }: LngParams) {
 								d='m7 9 4-4-4-4M1 9l4-4-4-4'
 							/>
 						</svg>
-					</li>
-					<li className='flex items-center'>
-						<span className='flex items-center justify-center w-5 h-5 me-2 text-xs border border-gray-500 rounded-full shrink-0 dark:border-gray-400'>
-							2
-						</span>
-						Account <span className='hidden sm:inline-flex sm:ms-2'>Info</span>
-						<svg
-							className='w-3 h-3 ms-2 sm:ms-4 rtl:rotate-180'
-							aria-hidden='true'
-							xmlns='http://www.w3.org/2000/svg'
-							fill='none'
-							viewBox='0 0 12 10'
-						>
-							<path
-								stroke='currentColor'
-								stroke-linecap='round'
-								stroke-linejoin='round'
-								stroke-width='2'
-								d='m7 9 4-4-4-4M1 9l4-4-4-4'
-							/>
-						</svg>
-					</li>
-					<li className='flex items-center'>
-						<span className='flex items-center justify-center w-5 h-5 me-2 text-xs border border-gray-500 rounded-full shrink-0 dark:border-gray-400'>
-							3
-						</span>
-						Review
 					</li>
 				</ol>
 			</div>
